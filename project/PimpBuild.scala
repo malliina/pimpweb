33fcdc6e46a3e3c1f8543f35d9f9cda2b831c500
malliina <malliina123@gmail.com>
2015-04-06 20:33:06 +0300

    use bintray

--- project/PimpBuild.scala
+++ project/PimpBuild.scala
@@ -12,16 +12,17 @@ object PimpBuild extends Build {
   lazy val pimpWeb = PlayProjects.plainPlayProject("pimpweb").enablePlugins(BuildInfoPlugin).settings(pimpSettings: _*)
 
   lazy val commonSettings = Seq(
-    version := "1.4.0",
+    version := "1.4.1",
     scalaVersion := "2.11.6",
     libraryDependencies ++= deps,
     retrieveManaged := false,
     fork in Test := true,
-    resolvers += "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases/"
+    resolvers ++= Seq(
+      "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases/",
+      sbt.Resolver.jcenterRepo,
+      "Bintray malliina" at "http://dl.bintray.com/malliina/maven")
   )
-  lazy val pimpSettings = commonSettings ++
-    herokuSettings ++
-    buildMetaSettings
+  lazy val pimpSettings = commonSettings ++ herokuSettings ++ buildMetaSettings
 
   def herokuSettings = HerokuPlugin.settings ++ Seq(
     HerokuKeys.heroku := Paths.get( """C:\Program Files (x86)\Heroku\bin\heroku.bat""")
@@ -35,8 +36,8 @@ object PimpBuild extends Build {
   val mleGroup = "com.github.malliina"
 
   lazy val deps = Seq(
-    mleGroup %% "util-azure" % "1.8.0",
-    mleGroup %% "play-base" % "0.4.0",
+    mleGroup %% "util-azure" % "1.8.1",
+    mleGroup %% "play-base" % "0.4.1",
     "com.newrelic.agent.java" % "newrelic-agent" % "2.15.1" % "provided"
   )
 }
--- project/project/WebBuildBuild.scala
+++ project/project/WebBuildBuild.scala
@@ -16,7 +16,7 @@ object WebBuildBuild extends Build {
   def sbtPlugins = Seq(
     "com.typesafe.play" % "sbt-plugin" % "2.3.8",
     "com.github.malliina" %% "sbt-paas-deployer" % "1.0.0",
-    "com.github.malliina" %% "sbt-play" % "0.1.1",
+    "com.github.malliina" %% "sbt-play" % "0.2.0",
     "com.eed3si9n" % "sbt-buildinfo" % "0.4.0"
   ) map addSbtPlugin
 
