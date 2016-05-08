a27bf3bc107a6b729583cb018125dd6906a60a13
Michael Skogberg <malliina123@gmail.com>
2016-05-08 19:54:55 +0300

    Use https

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -16,7 +16,7 @@ object Home {
     val url = toUrl(fileName)
   }
 
-  val downloadBaseUrl = "http://files.musicpimp.org/files/"
+  val downloadBaseUrl = "https://files.musicpimp.org/files/"
   val version = "3.0.0"
   val msiDownload = Download(s"musicpimp-$version.msi")
   val debDownload = Download(s"musicpimp_${version}_all.deb")
@@ -25,7 +25,7 @@ object Home {
   val latestDownloads = Seq(msiDownload, debDownload, rpmDownload, dmgDownload)
   val linuxConfFile = "/opt/musicpimp/musicpimp.conf"
   val windowsConfFile = """C:\Program Files (x86)\MusicPimp\musicpimp.conf"""
-  val winPhoneAppUri = "http://www.windowsphone.com/s?appid=84cd9030-4a5c-4a03-b0ab-4d59c2fa7d42"
+  val winPhoneAppUri = "https://www.windowsphone.com/s?appid=84cd9030-4a5c-4a03-b0ab-4d59c2fa7d42"
   val winStoreAppUri = "http://apps.microsoft.com/windows/en-us/app/musicpimp/73b9a42c-e38a-4edf-ac7e-00672230f7b6"
   val androidAppUri = "https://play.google.com/store/apps/details?id=org.musicpimp"
   val amazonAppUri = "http://www.amazon.com/gp/product/B00GVHTEJY/ref=mas_pm_musicpimp"
