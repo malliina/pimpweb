df6d2d7edab717844f00f0437d7d327eb0e951fe
Michael Skogberg <michael.skogberg@outlook.com>
2017-03-26 21:57:14 +0300

    Add test

---
+++ test/com/malliina/pimpweb/tags/MarkdownTests.scala
@@ -0,0 +1,10 @@
+package com.malliina.pimpweb.tags
+
+import org.scalatest.FunSuite
+
+class MarkdownTests extends FunSuite {
+  test("can parse markdown") {
+    PimpTags.api
+    assert(1 === 1)
+  }
+}
